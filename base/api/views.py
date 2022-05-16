7262ed3c2e58e0c93da2e39132fa7a606e0dc8c5
Nguyen Minh Hoang - B19DCVT158 <92423005+eyeoflol843@users.noreply.github.com>
2022-05-16 16:27:49 +0700

    Nguyen Minh Hoang

--- base/api/views.py
+++ base/api/views.py
@@ -13,3 +13,17 @@ def getRoutes(request):
         'GET /api/rooms/:id'
     ]
     return Response(routes)
+
+
+@api_view(['GET'])
+def getRooms(request):
+    rooms = Room.objects.all()
+    serializer = RoomSerializer(rooms, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    serializer = RoomSerializer(room, many=False)
+    return Response(serializer.data)
