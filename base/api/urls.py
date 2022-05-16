fd9d849b0927408370c712d33ed176db8f01b1c4
Nguyen Minh Hoang - B19DCVT158 <92423005+eyeoflol843@users.noreply.github.com>
2022-05-16 16:23:46 +0700

    Nguyen Minh Hoang

--- base/api/urls.py
+++ base/api/urls.py
@@ -1,8 +1,4 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('',  views.getRoutes),
-    path('rooms/', views.getRooms),
-    path('rooms/<str:pk>/', views.getRoom),
-]
+
