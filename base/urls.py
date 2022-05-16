3389348606db1bae9c87ff5f9e9b335cc221ee88
Tanlamlo <105580680+Tanlamlo@users.noreply.github.com>
2022-05-16 22:04:44 +0700

    Update urls.py

--- base/urls.py
+++ base/urls.py
@@ -5,3 +5,10 @@ urlpatterns = [
     path('login/', views.loginPage, name="login"),
     path('logout/', views.logoutUser, name="logout"),
     path('register/', views.registerPage, name="register"),
+    
+    
+    path('room/<str:pk>/', views.room, name="room"),
+    path('create-room/<str:id_user>/', views.createRoom, name="create-room"),
+    path('update-room/<str:pk>/', views.updateRoom, name="update-room"),
+    path('delete-room/<str:pk>/', views.deleteRoom, name="delete-room"),
+    path('delete-message/<str:pk>/', views.deleteMessage, name="delete-message"),
