bceec27e67d40399269a1dc27bfdc30d74b15f61
Đào Hồng Sơn - B19DCVT307 <100818949+sonchemjoko@users.noreply.github.com>
2022-05-17 21:22:36 +0700

    Update urls.py

--- base/urls.py
+++ base/urls.py
@@ -12,3 +12,11 @@ urlpatterns = [
     path('update-room/<str:pk>/', views.updateRoom, name="update-room"),
     path('delete-room/<str:pk>/', views.deleteRoom, name="delete-room"),
     path('delete-message/<str:pk>/', views.deleteMessage, name="delete-message"),
+    
+    #Sơn update
+    path('profile/<str:pk>/', views.userProfile, name="user-profile"),
+    path('update-user/', views.updateUser, name="update-user"),
+    path('topics/', views.topicsPage, name="topics"),
+    path('activity/', views.activityPage, name="activity"),
+    path('', views.home, name="home"),
+]
