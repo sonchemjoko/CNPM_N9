63584d39f64ba15197b64c78e1a2f565460b7b08
Nguyen Minh Hoang - B19DCVT158 <92423005+eyeoflol843@users.noreply.github.com>
2022-05-17 21:12:35 +0700

    Update views.py

--- base/views.py
+++ base/views.py
@@ -12,30 +12,7 @@ from .forms import RoomForm
 #     {'id': 3, 'name': 'Frontend developers'},
 # ]
 
-def loginPage(request):
-    page = 'login'
-    if request.user.is_authenticated:
-        return redirect('home')
 
-    if request.method == 'POST':
-        email = request.POST.get('email').lower()
-        password = request.POST.get('password')
-
-        try:
-            user = User.objects.get(email=email)
-        except:
-            messages.error(request, 'Không Tồn Tại Tài Khoản Này')
-
-        user = authenticate(request, email=email, password=password)
-
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            messages.error(request, 'Sai Tên Đăng Nhập Hoặc Mật Khẩu')
-
-    context = {'page': page}
-    return render(request, 'base/login_register.html', context)
 
 
 def room(request, pk):
