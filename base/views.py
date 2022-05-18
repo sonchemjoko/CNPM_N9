495f2edfc69f275b6fcb1ec8f7da10192a452d25
Nguyen Minh Hoang - B19DCVT158 <92423005+eyeoflol843@users.noreply.github.com>
2022-05-18 14:43:40 +0700

    Update views.py

--- base/views.py
+++ base/views.py
@@ -10,7 +10,51 @@ from .forms import RoomForm, UserForm, MyUserCreationForm
 # Create your views here.
 
 
+def loginPage(request):
+    page = 'login'
+    if request.user.is_authenticated:
+        return redirect('home')
 
+    if request.method == 'POST':
+        email = request.POST.get('email').lower()
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(email=email)
+        except:
+            messages.error(request, 'Không Tồn Tại Tài Khoản Này')
+
+        user = authenticate(request, email=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Sai Tên Đăng Nhập Hoặc Mật Khẩu')
+
+    context = {'page': page}
+    return render(request, 'base/login_register.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
+
+
+def registerPage(request):
+    form = MyUserCreationForm()
+
+    if request.method == 'POST':
+        form = MyUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Đã xảy ra lỗi khi đăng ký')
+
+    return render(request, 'base/login_register.html', {'form': form})
 
 
 def home(request):
