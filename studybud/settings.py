4a8b466b760ac162734c07c380565584c4eac1bf
Đào Hồng Sơn - B19DCVT307 <100818949+sonchemjoko@users.noreply.github.com>
2022-05-21 23:03:54 +0700

    Update settings.py

--- studybud/settings.py
+++ studybud/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -132,9 +132,12 @@ DATETIME_FORMAT = 'd-m-Y H:i:s'
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
 
